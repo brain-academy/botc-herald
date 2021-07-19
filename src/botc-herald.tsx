283b632cca65c2fce8fd1ec82e59d8c07e2fcbdb
David Urvoy <david.urvoy.pro@gmail.com>
2021-07-19 18:27:04 +0000

    build: configure project as npm library

---
+++ src/botc-herald.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import BotcInvite from './components/botc-invite.component'
+
+export default function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<BotcInvite />
+			</header>
+		</div>
+	)
+}
